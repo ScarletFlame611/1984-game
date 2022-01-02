d0e5545fe925cfd776636e961d941921cb2cedd1
WTFDOYOUWANT <varya.nika@yandex.ru>
2022-01-02 08:33:29 +0300

    Creating fight v0.1

--- game.py
+++ game.py
@@ -1,8 +1,6 @@
 import pygame
+from tutor_level import *
 
 
 def start(screen):
-    screen.fill((10, 10, 10))
-    font = pygame.font.Font(None, 50)
-    font = font.render("Тут как бы игра начинается", True, (100, 100, 100))
-    screen.blit(font, (150, 180))
+    game()
