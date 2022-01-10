ece380de02e30397b6dcc7b591b814942f5c3c16
ScarletFlame611 <kseniya.uryasyeva@mail.ru>
2022-01-10 16:32:17 +0300

    coins and heals added

--- tutor_level.py
+++ tutor_level.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 import time
+import random
 
 size = width, height = 770, 420
 pygame.init()
@@ -16,18 +17,27 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 wall_group = pygame.sprite.Group()  # группа для остановки игрока при столкновении с ее объектами
+bonus_group = pygame.sprite.Group()
+coins = pygame.sprite.Group()
+
 
 # генерация уровня
-def generate_level(level):
+def generate_level(level, number_of_level=0):
     new_player, x, y = None, None, None
+    tiles_types = ['empty', 'wall']
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
-                Tile('empty', x, y)
+                Tile(tiles_types[0], x, y)
             elif level[y][x] == '#':
-                Tile('wall', x, y)
+                Tile(tiles_types[1], x, y)
+            elif level[y][x] == '+':
+                HealTile(x, y)
+            elif level[y][x] == '$':
+                Tile(tiles_types[0], x, y)
+                Coin(x, y)
             elif level[y][x] == '@':
-                Tile('empty', x, y)
+                Tile(tiles_types[0], x, y)
                 new_player = Player(x, y)
     return new_player, x, y
 
@@ -51,16 +61,25 @@ def load_image(name, colorkey=None):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
 tile_images = {
     'wall': load_image('box.png'),
-    'empty': load_image('floor.png')
+    'empty': load_image('floor.png'),
+    'metal1': load_image('metal1.png')
 }
 player_image = load_image('player.png')
 
 tile_width = tile_height = 70
+
 
 # класс игрока
 class Player(pygame.sprite.Sprite):
@@ -77,6 +96,8 @@ class Player(pygame.sprite.Sprite):
             self.goframes.append(load_image(f"go{i}.png"))
         self.cur_frame = 0
         self.current_state = "right"  # текущее направление, куда смотрит игрок
+        self.hp = 150
+        self.score = 0
 
     # передаются координаты сдвига игрока и направление движения
     # (0-вправо, 1 - влево, -1 - остановка)
@@ -103,6 +124,12 @@ class Player(pygame.sprite.Sprite):
             if pygame.sprite.spritecollideany(self, wall_group):
                 # если произошло столкновение со стеной - перемещаем обратно
                 self.rect.move_ip([-i for i in coords])
+            hits = pygame.sprite.spritecollide(self, bonus_group, False)
+            if hits:
+                for sprite in hits:
+                    if pygame.sprite.collide_rect(sprite, sprite):
+                        sprite.buff(self)
+
         else:
             self.image = player_image if self.current_state == "right" else pygame.transform.flip(
                 player_image, True, False)
@@ -131,6 +158,60 @@ class Border(pygame.sprite.Sprite):
             self.add(wall_group)
             self.image = pygame.Surface([x2 - x1, 1])
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+class HealTile(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = load_image('heal.png')
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.add(bonus_group)
+        self.used = False
+
+    def buff(self, other):
+        if not self.used:
+            other.hp += 300
+            self.used = True
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(bonus_group, all_sprites)
+        self.frames = []
+        self.add(coins)
+        for i in range(1, 7):
+            self.frames.append(load_image(f"coin{i}.png"))
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 20, tile_height * pos_y + 15)
+
+    def update(self):
+        amount_of_frames = 6
+        self.cur_frame = int(
+            (time.time() - start_frame) * frames_per_second % amount_of_frames)
+        self.image = self.frames[self.cur_frame]
+        self.used = False
+
+    def buff(self, other):
+        if not self.used:
+            other.score += 300
+            self.used = True
+            self.remove(coins)
+            self.remove(bonus_group)
+
+
+def draw_bar(surf, x, y, player_hp):
+    if player_hp < 0:
+        player_hp = 0
+    bar_width = 300
+    bar_height = 10
+    fill = (player_hp / 1500) * bar_width
+    outline_rect = pygame.Rect(x, y, bar_width, bar_height)
+    fill_rect = pygame.Rect(x, y, fill, bar_height)
+    pygame.draw.rect(surf, pygame.Color("green"), fill_rect)
+    pygame.draw.rect(surf, pygame.Color("white"), outline_rect, 2)
 
 
 def terminate():
@@ -162,6 +243,9 @@ def game():
             player.update([0, 0], -1)
         screen.fill("black")
         tiles_group.draw(screen)
+        bonus_group.draw(screen)
+        coins.update()
+        draw_bar(screen, 230, 5, player.hp)
         player_group.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
